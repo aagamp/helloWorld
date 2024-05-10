9daf3f275f92a7d83e2e16582a92a5fae4cc3fc3
Aagam Parajuli <aagam28282@gmail.com>
2024-05-10 22:19:51 +0545

    first commit

---
+++ first.py
@@ -0,0 +1,20 @@
+# print('hello world')
+
+# for i in range(0,11):
+#     print('13 * {} = {}'.format(i,13*i))
+
+# print("Calculating Simple Intrest")
+
+# principle = int(input("Enter the value of Principle"))
+# rate = int(input("Enter the value rate in percentage"))
+# time = int(input("Enter the time in years"))
+
+# SI = principle*time*rate/100
+
+# print('The simple intrest of given values is: Rs.{}'.format(SI))
+
+v = 2
+if(v == 1):
+    print("inside if statement")
+else:
+    print("inside else statment")
